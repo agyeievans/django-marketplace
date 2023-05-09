5e0ea4b7b86af8678de99e9c3d736e07decfa002
agyeievans <evanskofiagyei06@gmail.com>
2023-05-09 02:33:13 +0000

    create category model and migrate

--- item/models.py
+++ item/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    # create model name and show categories name
+    class Meta:
+        # order by name
+        ordering = ('name',)
+        verbose_name_plural = 'Categories'
+    def __str__(self):
+        return self.name
