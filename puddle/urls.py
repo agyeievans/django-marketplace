9224e7d4cda4d4b440c10dac29897a3a2335c61f
agyeievans <evanskofiagyei06@gmail.com>
2023-05-09 01:47:28 +0000

    added block content,title and contact

--- puddle/urls.py
+++ puddle/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index
+from core.views import index, contact
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('contact/', contact, name='contact'),
 ]
