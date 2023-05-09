a17f069a20016e75d4edb531050ce290a10e5d6f
agyeievans <evanskofiagyei06@gmail.com>
2023-05-09 16:57:03 +0000

    added related items

--- puddle/urls.py
+++ puddle/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import index, contact
 from django.conf import settings
 from django.conf.urls.static import static
@@ -9,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
+    path('item/', include('item.urls', namespace='item')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
