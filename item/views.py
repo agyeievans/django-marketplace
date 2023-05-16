13b781b8948345bab5e924446ae73d30a4244634
agyeievans <evanskofiagyei06@gmail.com>
2023-05-16 13:40:57 +0000

    add edit item

--- item/views.py
+++ item/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from item.models import Item
 from django.contrib.auth.decorators import login_required
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
 def detail(request, pk):
@@ -35,3 +35,17 @@ def delete(request, pk):
     item.delete()
     
     return redirect('dashboard:index')
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+        
+    return render(request, 'item/form.html', {'form': form, 'title': 'Edit Item',})
